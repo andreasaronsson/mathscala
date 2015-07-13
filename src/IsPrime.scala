132bdfbef5321f62d3138882b88d84958bb499af
Andreas Aronsson <aron@aron.nu>
2015-07-13 20:04:18 +0200

    Extend App

--- src/IsPrime.scala
+++ src/IsPrime.scala
@@ -1,19 +1,21 @@
-
-
 import scala.Array.canBuildFrom
 
-object IsPrime {
+object IsPrime extends App {
 
   def isPrime(n: Int): Boolean = {
     !(2 to (n - 1)).exists(n % _ == 0)
   }
 
-  def main(args: Array[String]) {
+  def parseArgsToInts(args: Array[String]): Array[Int] = {
     val nargs = args map { a => a filterNot (x => x == ',') }
-    val intargs = nargs map (_.toInt)
-    intargs foreach {
-      case x if isPrime(x) => println(x)
-      case _ => Nil
-    }
+    nargs map (_.toInt)
   }
+
+  val intargs = parseArgsToInts(args)
+  intargs foreach {
+    case x if isPrime(x) => println(x)
+    case _ => Nil
+  }
+
 }
+IsPrime.main(args)
